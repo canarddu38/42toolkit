bfc331031be4aa8a1008173cfa8798ffc594986f
0x262d <71982379+canarddu38@users.noreply.github.com>
2025-07-13 19:58:32 +0200

    Add files via upload

---
+++ customShell/colors.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   colors.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcleme <julcleme@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 18:40:50 by julcleme          #+#    #+#             */
+/*   Updated: 2025/07/13 19:04:55 by julcleme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLORS_H
+#define COLORS_H
+
+#define COLOR_BG		"\e[38;5;235m"
+#define COLOR_RED1		"\e[38;5;124m"
+#define COLOR_GREEN1	"\e[38;5;106m"
+#define COLOR_YELLOW1	"\e[38;5;172m"
+#define COLOR_BLUE1		"\e[38;5;66m"
+#define COLOR_PURPLE1	"\e[38;5;132m"
+#define COLOR_AQUA1		"\e[38;5;72m"
+#define COLOR_GRAY1		"\e[38;5;246m"
+
+#define COLOR_RED2		"\e[38;5;167m"
+#define COLOR_GREEN2	"\e[38;5;142m"
+#define COLOR_YELLOW2	"\e[38;5;214m"
+#define COLOR_BLUE2		"\e[38;5;109m"
+#define COLOR_PURPLE2	"\e[38;5;175m"
+#define COLOR_AUQUA2	"\e[38;5;108m"
+
+#endif
--- customShell/main.c
+++ customShell/main.c
@@ -1,1 +1,51 @@
-m
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcleme <julcleme@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 17:53:06 by julcleme          #+#    #+#             */
+/*   Updated: 2025/07/13 19:03:28 by julcleme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "colors.h"
+
+#define CMD_MAX        256
+#define PROMPT_MAX     64
+#define BANNER		   ##COLORS_BG##"\
+\t                   __                          __         ____\n\
+\t  _______  _______/ /_____  ____ ___     _____/ /_  ___  / / /\n\
+\t / ___/ / / / ___/ __/ __ \\/ __ `__ \\   / ___/ __ \\/ _ \\/ / / \n\
+\t/ /__/ /_/ (__  ) /_/ /_/ / / / / / /  (__  ) / / /  __/ / /  \n\
+\t\\___/\\__,_/____/\\__/\\____/_/ /_/ /_/  /____/_/ /_/\\___/_/_/   \n"
+
+void	prompt_print(char	*str)
+{
+	printf("%s", str);
+}
+
+int	main(void)
+{
+	char	command[CMD_MAX];
+	char	prompt[PROMPT_MAX] = "julcleme@z1r11p3 [/home/julcleme/piscine/test]$ ";
+
+	printf("%s", BANNER);
+	while (1)
+	{
+		prompt_print(prompt);
+		fgets(command, CMD_MAX, stdin);
+		if (!strncmp(command, "exit", 4))
+			break;
+		else
+			system(command);
+	}
+
+	printf("Closing...");
+	return (0);
+}
